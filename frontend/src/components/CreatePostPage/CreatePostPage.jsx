f135bfd376bb1bab957e674dd387e9534b8d6f6e
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-11 10:36:38 +0000

    Add the sidebar

--- frontend/src/components/CreatePostPage/CreatePostPage.jsx
+++ frontend/src/components/CreatePostPage/CreatePostPage.jsx
@@ -1,5 +1,6 @@
 function CreatePostPage() {
     return <>
+    hula
     </>
 }
 
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,11 +2,13 @@ import App from './components/App/App'
 import ErrorPage from './components/ErrorPage/ErrorPage'
 import LoginPage from './components/LoginPage/LoginPage'
 import Signup from './components/SignupPage/Signup'
+import CreatePostPage from './components/CreatePostPage/CreatePostPage'
 
 const routes = [
     { path: '/', element: <App />, errorElement: <ErrorPage /> },
     { path: '/login', element: <LoginPage /> },
     { path: '/signup', element: <Signup /> },
+    { path: '/post/create', element: <CreatePostPage /> },
 ]
 
 export default routes
