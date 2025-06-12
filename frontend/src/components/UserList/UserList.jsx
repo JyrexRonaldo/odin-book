3d3f9539b8f362ae0c21c5c7cb88c6a314271833
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-12 20:45:30 +0000

    Add user list component

---
+++ frontend/src/components/UserList/UserList.jsx
@@ -0,0 +1,10 @@
+function UserList() {
+    return (
+        <div>
+            <div className="bg-amber-700"><input type="search" name="" id="" /></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default UserList
