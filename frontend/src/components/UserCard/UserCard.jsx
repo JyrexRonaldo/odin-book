b02ac5be5afbbd1d2b3711e3b298150266b2f9eb
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-12 20:35:13 +0000

    Add user card component

---
+++ frontend/src/components/UserCard/UserCard.jsx
@@ -0,0 +1,17 @@
+function UserCard() {
+    return (
+        <div className="size-72 bg-blue-900 rounded-xl text-white">
+            <img src="" alt="" />
+            <p>jyrex</p>
+            <p>5 followers</p>
+            <p>@IAmJyrex</p>
+            <p></p>
+            <div>
+                <button>Follow</button>
+                <button>View</button>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard
