266f0811e821716b8e9b7fb74c0abccf9186ff75
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-18 23:18:27 +0000

    Add explore component

---
+++ frontend/src/components/Explore/Explore.jsx
@@ -0,0 +1,12 @@
+import PostListComponent from '../PostListComponent/PostListComponent'
+
+function Explore() {
+    return (
+        <>
+            <div className='text-white'>Explore</div>
+            <PostListComponent />
+        </>
+    )
+}
+
+export default Explore
