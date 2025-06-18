30bda7bf3bfdb1df68c4b6073dea6b838f0ec153
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-18 23:18:51 +0000

    Add feed component

---
+++ frontend/src/components/Feed/Feed.jsx
@@ -0,0 +1,12 @@
+import PostListComponent from '../PostListComponent/PostListComponent'
+
+function Feed() {
+    return (
+        <>
+            <div className="text-white">Feed</div>
+            <PostListComponent />
+        </>
+    )
+}
+
+export default Feed
