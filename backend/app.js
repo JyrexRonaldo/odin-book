d7d8727f0f1887f86e67eaeec10245ad804e410a
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-20 14:01:55 +0000

    Add authentication routes and controllers

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use("/auth", authRouter);
+app.use("/", postRouter);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
---
+++ backend/routes/authRouter.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const authRouter = Router();
+const authController = require("../controllers/authController");
+
+authRouter.route("/signup").post(authController.createUser);
+
+authRouter.route("signin").post(authController.handleSignIn);
