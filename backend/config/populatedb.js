95df28892bee576437267aaf9ecb4e1d35cf9365
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-22 08:29:18 +0000

    Add script for seeding database with fake data

---
+++ backend/config/populatedb.js
@@ -0,0 +1,77 @@
+const { faker } = require("@faker-js/faker");
+const prisma = require("./prisma");
+const asyncHandler = require("express-async-handler");
+
+const usersData = Array.from({ length: 10 }).map((element, index) => {
+  element = {
+    name: faker.person.fullName(),
+    username: faker.internet.username(),
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+    bio: faker.person.bio(),
+  };
+  return element;
+});
+
+const postsData = Array.from({ length: 20 }).map((element, index) => {
+  let authorId = index + 1;
+//   let status = false;
+  if (authorId > 10) {
+    authorId -= 10;
+  }
+//   if (authorId % 2 === 0) {
+//     status = true;
+//   }
+
+  element = {
+    body: faker.lorem.paragraph(),
+    authorId,
+  };
+  return element;
+});
+
+const commentsData = Array.from({ length: 20 }).map((element, index) => {
+  let authorId = index + 1;
+  if (authorId > 10) {
+    authorId -= 10;
+  }
+  let postId = index + 1;
+  if (postId > 10) {
+    postId -= 10;
+  }
+  element = {
+    comment: faker.lorem.sentence(),
+    authorId,
+    postId,
+  };
+  return element;
+});
+
+const populateUserDb = asyncHandler(async () => {
+  await prisma.user.createMany({
+    data: usersData,
+  });
+  console.log({ usersData });
+});
+
+const populatePostDb = asyncHandler(async () => {
+  await prisma.post.createMany({
+    data: postsData,
+  });
+  console.log(postsData);
+});
+
+const populateCommentDb = asyncHandler(async () => {
+  await prisma.comment.createMany({
+    data: commentsData,
+  });
+  console.log(commentsData);
+});
+
+const populatedb = asyncHandler(async () => {
+  await populateUserDb();
+  await populatePostDb();
+  await populateCommentDb();
+});
+
+populatedb();
