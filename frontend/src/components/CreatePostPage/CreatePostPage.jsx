ddc700e54dc4cc012e124bbfb7663d36b9fce33f
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-07-12 07:21:29 +0000

    Add image upload functionality

--- frontend/src/components/CreatePostPage/CreatePostPage.jsx
+++ frontend/src/components/CreatePostPage/CreatePostPage.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react'
 
 function CreatePostPage() {
     const [postBody, setPostBody] = useState('')
+    // const [file, setFile] = useState(null)
+    const [selectedImgUrl, setSelectedImgUrl] = useState(null)
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -18,7 +20,7 @@ function CreatePostPage() {
     }
 
     function handleCancelButton() {
-        navigate('/')
+        navigate('/explore')
     }
 
     async function handleCreatePostButton() {
@@ -49,6 +51,20 @@ function CreatePostPage() {
         }
     }
 
+    function handleImageSelector(e) {
+        console.log(e.target.files[0])
+        const file = e.target.files[0]
+        if (file) {
+            const fileReader = new FileReader()
+            fileReader.readAsDataURL(file)
+            // fileReader.onload = console.log(this)
+            fileReader.addEventListener('load', function () {
+                console.log(this.result)
+                setSelectedImgUrl(this.result)
+            })
+        }
+    }
+
     return (
         <>
             <div className="flex h-screen flex-col items-center justify-start gap-10 p-5">
@@ -57,11 +73,11 @@ function CreatePostPage() {
                     <p className="text-5xl font-bold">Create Post</p>
                     <div className="flex flex-col gap-3">
                         <p className="text-2xl font-bold">Image</p>
-                        <div className="flex min-h-56 justify-center rounded-lg bg-blue-500 p-5 text-center">
+                        <div className="relative flex min-h-56 justify-center rounded-lg bg-blue-500 p-5 text-center">
                             <div className="flex flex-col items-center justify-between">
                                 <p>Drag Here to Upload Media</p>
                                 <p>
-                                    Should be less than 9mb and only ong and jpg
+                                    Should be less than 9mb and only png and jpg
                                     files
                                 </p>
                                 <p>Recommended size:</p>
@@ -70,13 +86,23 @@ function CreatePostPage() {
                                 <label htmlFor="postImage">Browse Files</label>
                             </div>
                             <input
+                                onChange={handleImageSelector}
+                                // files={}
                                 type="file"
                                 name="postImage"
                                 id="postImage"
-                                className="hidden"
+                                accept="image/*"
+                                className="absolute top-0 h-full w-full bg-red-600/0 text-fuchsia-700"
                             />
                         </div>
                     </div>
+                    {/* <div> */}
+                    { selectedImgUrl &&  <img
+                        className="h-auto w-full rounded-lg"
+                        src={selectedImgUrl}
+                        alt=""
+                    />}
+                    {/* </div> */}
                     <div className="flex flex-col gap-3">
                         <label
                             className="text-2xl font-bold"
