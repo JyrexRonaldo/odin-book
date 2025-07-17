582f9c6ae0089724db2876019d3af8d3870ebd92
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-07-17 17:13:59 +0000

    Add text input component

---
+++ frontend/src/components/TextInput/TextInput.jsx
@@ -0,0 +1,21 @@
+import { VscSend } from 'react-icons/vsc'
+import { MdOutlineInsertEmoticon } from "react-icons/md";
+
+
+function TextInput() {
+    return (
+        <div className=" mx-auto mb-5 grid max-w-11/12 grid-cols-[min-content_auto_min-content] items-center gap-4 rounded-lg bg-blue-900 p-4 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-blue-900">
+            <button><MdOutlineInsertEmoticon /></button>
+            <input
+                type="text"
+                placeholder="Type message here 😉"
+                className="mr-3 w-full bg-blue-900 focus:outline-none"
+            />
+            <button>
+                <VscSend />
+            </button>
+        </div>
+    )
+}
+
+export default TextInput
