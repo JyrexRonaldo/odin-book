120300b2d247846971be05d4d097e2b0e841cf00
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-07-17 11:32:18 +0000

    Set up message view layout

--- frontend/src/components/MessageView/MessageView.jsx
+++ frontend/src/components/MessageView/MessageView.jsx
@@ -1,5 +1,10 @@
 function MessageView() {
-return <div>MessageView</div>
+    return (
+        <div className="h-screen bg-red-600 text-white lg:grid lg:grid-cols-[1fr_2fr]">
+            <div className="bg-green-600 max-lg:hidden">MessageView </div>
+            <div className="bg-yellow-600"> Hello</div>
+        </div>
+    )
 }
 
 export default MessageView
